a32c052f3cf4c043ac17c3b72c0e6414ae3ad4ba
Sayhan Ahmed <sayhan@ewnbd.com>
2025-08-12 12:55:12 +0600

    resoving build fixes

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ app.add_url_rule('/reset-password',
                 methods=['POST'], endpoint='reset_password')
 
 
-# import os
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
